61fecb40abd091e72c53c9985686d09b06094636
shiri134 <kshiri@thoughtworks.com>
2017-06-12 17:05:55 +0530

    Added Sign up functionality

--- app/Screens/SignUp.js
+++ app/Screens/SignUp.js
@@ -5,25 +5,59 @@ export default class SignUp extends Component {
     static navigationOptions = {
         title: 'Sign Up',
     };
+    constructor(props) {
+        super(props);
+        this.state = {username: '',rollno: '',name:'',dob:'',section:'',password:'',message:''};
+
+    }
     onSubmit=()=>{
-        Alert.alert("Congeatulation ");
+        return fetch('http://localhost:3000/signup/', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: this.state.username,
+                rollno: this.state.rollno,
+                name: this.state.name,
+                dob: this.state.dob,
+                class: this.state.section,
+                password: this.state.password
+            })
+        })
+            .then((response) => {
+                this.setState({message: JSON.parse(response._bodyText).message})
+            })
+            .catch((error) => {
+                console.error(error);
+            });
     };
 
     render() {
         return (
             <View style={{flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
                 <View>
-                    <TextInput style={styles.userText} placeholder="User Name"/>
-                    <TextInput style={styles.userText} placeholder="Roll No"/>
-                    <TextInput style={styles.userText} placeholder="Name"/>
-                    <TextInput style={styles.userText} placeholder="Date Of Birth"/>
-                    <TextInput style={styles.userText} placeholder="User Name"/>
-                    <TextInput style={styles.userText} placeholder="Password"/>
+                    <TextInput style={styles.userText} placeholder="User Name" onChangeText={(username) => this.setState({username})}
+                               value={this.state.username} />
+                    <TextInput style={styles.userText} placeholder="Roll No" onChangeText={(rollno) => this.setState({rollno})}
+                               value={this.state.rollno}/>
+                    <TextInput style={styles.userText} placeholder="Name" onChangeText={(name) => this.setState({name})}
+                               value={this.state.name}/>
+                    <TextInput style={styles.userText} placeholder="Date Of Birth" onChangeText={(dob) => this.setState({dob})}
+                               value={this.state.dob}/>
+                    <TextInput style={styles.userText} placeholder="Class" onChangeText={(section) => this.setState({section})}
+                               value={this.state.section}/>
+                    <TextInput style={styles.userText} placeholder="Password" onChangeText={(password) => this.setState({password})}
+                               value={this.state.password}/>
                     <TextInput style={styles.userText} placeholder="Confirm Password"/>
                 </View>
                 <Button onPress={this.onSubmit} color="red"
                         accessibilityLabel="See an informative alert" title={"Sign Up"}/>
+                <Text style={{color: "green"}}> {this.state.message}</Text>
             </View>
+
+
         );
     }
 }
