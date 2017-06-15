8f70496cdd1c2ce851c969e8d290310774fa8427
salauddin <snalband@thoughtworks.com>
2017-06-15 16:10:57 +0530

    added functionality to clear stck when signout

--- app/Screens/home.js
+++ app/Screens/home.js
@@ -4,17 +4,25 @@ import {
     Text,
     View, TextInput, Button, Alert,AsyncStorage
 } from 'react-native';
+import {NavigationActions} from "react-navigation";
 
 
 const STORAGE_KEY='id_token';
 export default class home extends Component {
       onSignOut= async ()=> {
           let value=await AsyncStorage.getItem(STORAGE_KEY);
-          if(value){
-          await AsyncStorage.removeItem(STORAGE_KEY);
-          const {navigate} = this.props.navigation;
-          navigate('signin');}
-    };
+          if(value) {
+              await AsyncStorage.removeItem(STORAGE_KEY);
+              this.props
+                  .navigation
+                  .dispatch(NavigationActions.reset(
+                      {
+                          index: 0,
+                          actions: [
+                              NavigationActions.navigate({ routeName: 'signin'})
+                          ]
+                      }));
+          }    };
 
 
     render(){
