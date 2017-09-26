4b5cf604ee0fead04d8107de759a20fa3fb4b439
salauddin <snalband@thoughtworks.com>
2017-09-27 02:54:57 +0530

    Salauddin | cahnges to run app in Andriod

--- index.ios.js
+++ index.ios.js
@@ -1,17 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, {Component} from 'react';
+import React from 'react';
 import App from './app/App'
 
-import {
-    AppRegistry,
-} from 'react-native';
-
-
+import {AppRegistry,} from 'react-native';
 
 
 AppRegistry.registerComponent('JNV', () => App);
